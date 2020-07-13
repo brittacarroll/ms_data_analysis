6a1041caaf8760a7ef23aebe0d1d5a380634c3b1
Britta Carroll <brittacarroll@gmail.com>
2020-07-13 17:01:10 -0400

    ran autopep8

--- minkowski_analysis.py
+++ minkowski_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import sys, getopt
+import sys
+import getopt
 import pdb
 import numpy as np
 from scipy.spatial import distance
@@ -15,7 +16,9 @@ data_file = pd.read_excel(f'{sys.argv[-1]}')
 # TODO: read two excel files, and concatenate data
 # data_file_2 = pd.read_excel
 
-#creates list with patients within range of min and max age, <6ml lesion size
+# creates list with patients within range of min and max age, <6ml lesion size
+
+
 def create_subject_list():
     subject_list = []
     for subject_num in (range(0, data_file.index[-1] + 1)):
@@ -24,14 +27,14 @@ def create_subject_list():
             continue
 
         age = data_file.loc[subject_num, 'AGE']
-        if not age or age <=MIN_AGE or age >= MAX_AGE:
+        if not age or age <= MIN_AGE or age >= MAX_AGE:
             continue
 
-        lesion_size = data_file.loc[subject_num,'Total_LL']
+        lesion_size = data_file.loc[subject_num, 'Total_LL']
         if not lesion_size < MAX_LESION_SIZE:
             continue
 
-        num = data_file.loc[subject_num, 'PATIENT'] 
+        num = data_file.loc[subject_num, 'PATIENT']
         sex = data_file.loc[subject_num, 'SEX']
         iq = data_file.loc[subject_num, 'IQ']
         edu_level = data_file.loc[subject_num, 'Edu_lev']
@@ -54,18 +57,20 @@ def create_patient_and_healthy_control_lists(subject_list):
             healthy_controls.append(subject)
         else:
             ms_patients.append(subject)
-    
+
     numpy_ms_patients = np.array(ms_patients)
     numpy_healthy_controls = np.array(healthy_controls)
 
     return numpy_ms_patients, numpy_healthy_controls
+
 
 def match_controls_with_patients(patient_list, control_list):
     # patient_healthy_control_data = np.empty((0,6))
     patient_healthy_control_data = []
 
     for control in control_list:
-        patient_closest_matches = patient_list[np.argsort(distance.cdist(np.atleast_2d(control), np.atleast_2d(patient_list), 'wminkowski', w=[0,5,5,5,2,0]))][0]
+        patient_closest_matches = patient_list[np.argsort(distance.cdist(np.atleast_2d(
+            control), np.atleast_2d(patient_list), 'wminkowski', w=[0, 5, 5, 5, 2, 0]))][0]
 
         matches = patient_closest_matches.tolist()
         # match_not_already_in_patient_hc_data = (patient_closest_matches - patient_healthy_control_data)[0]
@@ -85,24 +90,37 @@ def match_controls_with_patients(patient_list, control_list):
 
     return patient_healthy_control_data
 
-def create_excel_file(patient_healthy_control_data):
-    
 
-    format_data = pd.DataFrame(patient_healthy_control_data,
-                   columns=['PATIENT', 'AGE', 'SEX', 'IQ', 'Edu_lev', 'TOTAL LL', 
-                   'HC', 'HC-AGE', 'HC-SEX', 'HC-IQ', 'HC-Edu_lev', 'HC-TOTAL LL'])
+def create_excel_file(patient_healthy_control_data):
+
+    format_data = pd.DataFrame(
+        patient_healthy_control_data,
+        columns=[
+            'PATIENT',
+            'AGE',
+            'SEX',
+            'IQ',
+            'Edu_lev',
+            'TOTAL LL',
+            'HC',
+            'HC-AGE',
+            'HC-SEX',
+            'HC-IQ',
+            'HC-Edu_lev',
+            'HC-TOTAL LL'])
 
     format_data.index += 1
-    format_data.to_excel("new_results_diff_weights.xlsx")  
+    format_data.to_excel("new_results_diff_weights.xlsx")
+
 
 def main():
     subject_list = create_subject_list()
-    ms_patients, healthy_controls = create_patient_and_healthy_control_lists(subject_list)
-    patient_controls_matching_data = match_controls_with_patients(ms_patients, healthy_controls)
+    ms_patients, healthy_controls = create_patient_and_healthy_control_lists(
+        subject_list)
+    patient_controls_matching_data = match_controls_with_patients(
+        ms_patients, healthy_controls)
     create_excel_file(patient_controls_matching_data)
+
 
 if __name__ == "__main__":
     main()
-
-
-
